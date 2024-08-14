9883d608d417c4be7ff543603fa16f6048c69a76
vylia99 <110261644+vylia99@users.noreply.github.com>
2024-08-14 22:41:12 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import random
-import re
+
 
 def get_days_from_today(date):
     try:
         new_date = datetime.strptime(date, "%Y-%m-%d").date()
     except ValueError:
          print(f"Date format for '{date}' is incorrect. Expected format is 'YYYY-MM-DD'")
+         return
     finally:
         print('-'*30)
     
@@ -15,15 +16,19 @@ def get_days_from_today(date):
     
 def get_numbers_ticket(min, max, quantity):
     my_list = []
-    if min >= 1 and max <= 1000:       
-        while quantity >0:
-            number = random.randint(min, max)
-            if number in my_list:
-                quantity = quantity
-            else:
-                my_list.append(number) 
-                quantity -=1 
+    difference = max - min
+    if max < min or difference < quantity:
         return my_list
     else:
-        return  my_list
+        if min >= 1 and max <= 1000:       
+            while quantity >0:
+                number = random.randint(min, max)
+                if number not in my_list:
+                    my_list.append(number) 
+                    quantity -=1  
+                
+            my_list.sort()
+            return my_list
+        else:
+            return  my_list
     
